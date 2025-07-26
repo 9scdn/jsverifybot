dfc93dea1006fcb632a6f37b74a660a4976e5111
9scdn <lixiaolong2003@proton.me>
2025-07-26 14:30:30 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def load_config():
-    with open("config.json", "r", encoding="utf-8") as f:
-        return json.load(f)
-
-def is_official_account(username: str) -> bool:
-    config = load_config()
-    return username in config["public_accounts"] or username in config["hidden_accounts"]
